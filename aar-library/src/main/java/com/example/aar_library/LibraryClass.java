c8dda9795351ad9f28b52f7c24424d7eef6056ce
Aditya Srivastava <aditya.srivastav@jublfood.com>
2020-02-14 12:52:37 +0530

    aar file generation

---
+++ aar-library/src/main/java/com/example/aar_library/LibraryClass.java
@@ -0,0 +1,12 @@
+package com.example.aar_library;
+
+import android.util.Log;
+
+public class LibraryClass {
+
+    public static final String TAG = "LIBRARY_CLASS";
+
+    public static void message(String message){
+        Log.d(TAG, message);
+    }
+}
